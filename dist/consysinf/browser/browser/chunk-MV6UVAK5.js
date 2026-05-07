a1e975ff7c6f9eea94afff3b6eb13bcd27d952ed
Erick Condori <erick.mendoza7059@gmail.com>
2026-05-07 15:37:38 -0400

    config vercel final

---
+++ dist/consysinf/browser/browser/chunk-MV6UVAK5.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-OAK23WKC.js";import"./chunk-2UC55PAU.js";export{a as Servicios};
---
+++ dist/consysinf/browser/browser/chunk-R65KWWDH.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-GSZJCTAP.js";import"./chunk-HGDNM4HP.js";import"./chunk-2UC55PAU.js";export{a as Contactos};
